46e9eae93347c90459ec5b02cc7d71d1496e217a
jjrajani <jjrajani@gmail.com>
2016-05-02 12:12:30 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  birds = {}
+
+  data.each do |stat_cat, stat_value_array|
+    stat_value_array.each do |key2, value|
+      value.each do |key|
+        if birds.has_key?(key) == false
+          birds["#{key.to_s}"] = {}
+        end
+        if birds["#{key.to_s}"].has_key?(stat_cat) == false
+            birds["#{key.to_s}"][stat_cat] = []
+        end
+          birds["#{key.to_s}"][stat_cat].push(key2.to_s)
+      end
+    end
+  end
+  return birds
 end
